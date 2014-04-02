06ba92f2afc23bf5b0fbe035316a07100349ca6a
Instant Contiki <user@instant-contiki.(none)>
2014-04-02 11:48:28 +0200

    Adding script to automatic prepare vim configuration and addon

---
+++ prepare_vim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# get my configuration file
+wget -O ~/.vimrc https://raw.githubusercontent.com/ano1413/backup/master/dotfiles/.vimrc
+
+if [ -d "~/.vim" ]; then
+	mkdir ~/.vim/{autoload,bundle}
+fi
+
+# Install pathogen.vim 
+wget -P ~/.vim/autoload/ https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# Install autopair
+git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
+
+# Install snipmate
+git clone https://github.com/tomtom/tlib_vim.git ~/.vim/bundle/tlib_vim
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git ~/.vim/bundle/vim-addon-mw-utils
+git clone https://github.com/garbas/vim-snipmate.git ~/.vim/bundle/vim-snipmate
+git clone https://github.com/honza/vim-snippets.git ~/.vim/bundle/vim-snippets
